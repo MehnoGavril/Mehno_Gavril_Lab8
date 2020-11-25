291ecb41fc93f4c2f3b6b040f809ddf772830dc4
MIHAIL-GAVRIL MEHNO <gmehno@gmail.com>
2020-11-25 15:32:07 +0200

    Add project files.

---
+++ Data/Mehno_Gavril_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mehno_Gavril_Lab8.Models;
+
+namespace Mehno_Gavril_Lab8.Data
+{
+    public class Mehno_Gavril_Lab8Context : DbContext
+    {
+        public Mehno_Gavril_Lab8Context (DbContextOptions<Mehno_Gavril_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mehno_Gavril_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Mehno_Gavril_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Mehno_Gavril_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Mehno_Gavril_Lab8.Data;
+using Mehno_Gavril_Lab8.Models;
+
+namespace Mehno_Gavril_Lab8.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Mehno_Gavril_Lab8.Data.Mehno_Gavril_Lab8Context _context;
+
+        public IndexModel(Mehno_Gavril_Lab8.Data.Mehno_Gavril_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
