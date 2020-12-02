b1a39362529f1d6756cda305f983ec8d755ee728
MIHAIL-GAVRIL MEHNO <gmehno@gmail.com>
2020-12-02 15:31:11 +0200

    Am modificat si laboratorul trecut

--- Pages/Books/Edit.cshtml.cs
+++ Pages/Books/Edit.cshtml.cs
@@ -11,7 +11,7 @@ using Mehno_Gavril_Lab8.Models;
 
 namespace Mehno_Gavril_Lab8.Pages.Books
 {
-    public class EditModel : PageModel
+    public class EditModel : BookCategoriesPageModel
     {
         private readonly Mehno_Gavril_Lab8.Data.Mehno_Gavril_Lab8Context _context;
 
@@ -30,49 +30,53 @@ namespace Mehno_Gavril_Lab8.Pages.Books
                 return NotFound();
             }
 
-            Book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            Book = await _context.Book
+            .Include(b => b.Publisher)
+            .Include(b => b.BookCategories).ThenInclude(b => b.Category)
+            .AsNoTracking()
+               .FirstOrDefaultAsync(m => m.ID == id);
 
             if (Book == null)
             {
                 return NotFound();
             }
-            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
-            return Page();
+
+              PopulateAssignedCategoryData(_context, Book);
+
+               ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+                  return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.Attach(Book).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!BookExists(Book.ID))
+         // To protect from overposting attacks, enable the specific properties you want to bind to, for
+         // more details, see https://aka.ms/RazorPagesCRUD.
+         public async Task<IActionResult> OnPostAsync(int? id, string[] selectedCategories)
+         {
+                if (id == null)
                 {
-                    return NotFound();
+                   return NotFound();
                 }
-                else
-                {
-                    throw;
-                }
-            }
+                     var bookToUpdate = await _context.Book
+                    .Include(i => i.Publisher)
+                    .Include(i => i.BookCategories)
+                     .ThenInclude(i => i.Category)
+                    .FirstOrDefaultAsync(s => s.ID == id);
+              if (bookToUpdate == null)
+              {
+                  return NotFound();
+              }
 
-            return RedirectToPage("./Index");
-        }
-
-        private bool BookExists(int id)
-        {
-            return _context.Book.Any(e => e.ID == id);
-        }
+              if (await TryUpdateModelAsync<Book>( bookToUpdate, "Book", i => i.Title, i => i.Author, i => i.Price, i => i.PublishingDate, i => i.Publisher))
+              {
+                     UpdateBookCategories(_context, selectedCategories, bookToUpdate);
+                    await _context.SaveChangesAsync();
+                      return RedirectToPage("./Index");
+              }
+                        //Apelam UpdateBookCategories pentru a aplica informatiile din checkboxuri la entitatea Books care
+                         //este editata
+                  UpdateBookCategories(_context, selectedCategories, bookToUpdate);
+                  PopulateAssignedCategoryData(_context, bookToUpdate);
+                     return Page();
+         }
     }
 }
+
